20f97d7b2cabccef5bd14b0e9dc81db8315ce783
Matthias Haeussler <matthias.haeussler@novatec-gmbh.de>
2017-07-08 00:08:17 +0200

    new code

--- connection-service/src/main/kotlin/de/maeddes/connectionservice/ConnectionServiceApplication.kt
+++ connection-service/src/main/kotlin/de/maeddes/connectionservice/ConnectionServiceApplication.kt
@@ -3,16 +3,40 @@ package de.maeddes.connectionservice
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.mongodb.config.EnableMongoAuditing
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.repository.CrudRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.Instant
+import java.time.temporal.ChronoUnit
 import javax.persistence.Entity
 import javax.persistence.Id
 
 @SpringBootApplication
+@EnableMongoAuditing
 @RestController
-class ConnectionServiceApplication{
+class ConnectionServiceApplication(val directFlightConnectionRepository : DirectFlightConnectionRepository){
+
+    @RequestMapping("init")
+    fun init(){
+        directFlightConnectionRepository.save(
+                DirectFlightConnection(
+                        "Lufthansa",
+                        "STR",
+                        "FRA",
+                        Instant.now(),
+                        Instant.now().plus(1, ChronoUnit.HOURS)
+                )
+        )
+        directFlightConnectionRepository.save(DirectFlightConnection("Lufthansa", "HAM", "STR", Instant.now(), Instant.now().plus(2, ChronoUnit.HOURS))
+   }
+
+    @RequestMapping("clear")
+    fun clear() {
+        directFlightConnectionRepository.deleteAll()
+    }
 
 }
 
@@ -22,17 +46,17 @@ fun main(args: Array<String>) {
 
 @Entity
 class DirectFlightConnection(
-        @CreatedDate
-        var instantOfCreation: Instant,
         var airline: String,
-        var 
-        departureAirport: Any,
-        String,
-        arrivalAirport: Any String, departureTime: Any Instant, arrivalTime: Any Instant,
+        var departureAirport: String,
+        var destinationAirport: String,
+        var departureTime: Instant,
+        var arrivalTime: Instant,
         @Id
-        var id: String
-)
+        val id : String? = null,
+        @CreatedDate
+        var createdDate: Instant? = null
+        )
 
 @RepositoryRestResource(collectionResourceRel = "directFlightConnections", path = "direct-flight-connections")
-interface DirectFlightConnectionRepository : MongoRepository<DirectFlightConnection, String> {
-})
+interface DirectFlightConnectionRepository : MongoRepository<DirectFlightConnection, String>
+
---
+++ connection-service/src/test/kotlin/de/maeddes/connectionservice/ConnectionServiceApplicationTests.kt
@@ -0,0 +1,16 @@
+package de.maeddes.connectionservice
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ConnectionServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
