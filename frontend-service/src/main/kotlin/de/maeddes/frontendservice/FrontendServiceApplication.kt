0a2396acc408851e3eff059dc73243d1cb18e2a3
Matthias Haeussler <matthias.haeussler@novatec-gmbh.de>
2017-07-12 22:14:19 +0200

    new code

--- frontend-service/src/main/kotlin/de/maeddes/frontendservice/FrontendServiceApplication.kt
+++ frontend-service/src/main/kotlin/de/maeddes/frontendservice/FrontendServiceApplication.kt
@@ -3,8 +3,22 @@ package de.maeddes.frontendservice
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
+@EnableEurekaClient
 @SpringBootApplication
-class FrontendServiceApplication
+@EnableFeignClients
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+class FrontendServiceApplication : WebMvcConfigurerAdapter() {
+
+    @Bean
+    fun defaultSampler(): AlwaysSampler {
+        return AlwaysSampler()
+    }
+
+
+
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(FrontendServiceApplication::class.java, *args)
